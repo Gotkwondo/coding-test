372a306d2d06703f4e63f3a3f08a6ac26e3a43d0
Gotkwondo <dhwngus99@gmail.com>
2024-03-29 13:22:22 +0900

    ♻️ refactor(2 level): 이진 변환 반복하기 : 2진 변환, 구현, 정규식

--- Programmers/Level_2/이진 변환 반복하기.js
+++ Programmers/Level_2/이진 변환 반복하기.js
@@ -1,12 +1,25 @@
-const solution = s => {
-  let ocount = 0, count = 0;
+// const solution = s => {
+//   let ocount = 0, count = 0;
   
-  while (s.length > 1) {
-    let nlength = s.length;
-    s = s.replace(/0/g, "");
-    ocount += nlength - s.length;
-    s = s.length.toString(2);
-    count++
-  }
-  return [count, ocount];
+//   while (s.length > 1) {
+//     let nlength = s.length;
+//     s = s.replace(/0/g, "");
+//     ocount += nlength - s.length;
+//     s = s.length.toString(2);
+//     count++
+//   }
+//   return [count, ocount];
+// }
+
+const solution = (s) => {
+    let answer = [0, 0];
+    
+    while(s.length > '1'){
+        const l = s.length;
+        s = s.replace(/0/g, '');
+        answer[1] += l - s.length;
+        answer[0] += 1;
+        s = s.length.toString('2');
+    }
+    return answer;
 }
