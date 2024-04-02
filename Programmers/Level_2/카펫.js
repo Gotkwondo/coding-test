f1cf5157c21184ac275b72a3419a23bc7e3271b0
Gotkwondo <dhwngus99@gmail.com>
2024-04-02 10:15:42 +0900

    ✨ feat(2 level): 카펫 : 수학, 구현, 카펫 문제
    반복문과 분기 조건에 대한 정밀화

--- Programmers/Level_2/카펫.js
+++ Programmers/Level_2/카펫.js
@@ -1,11 +1,23 @@
+// const solution = (brown, yellow) => {
+//   const total = brown + yellow;
+//   for (let i = 3; i <= brown; i++) {
+//     if (total % i === 0) {
+//       const height = total / i;
+//       if ((height - 2) * (i - 2) === yellow) {
+//         return [height, i]
+//       }
+//     }
+//   }
+// };
+
 const solution = (brown, yellow) => {
+  let answer = [0, 0];
   const total = brown + yellow;
-  for (let i = 3; i <= brown; i++) {
-    if (total % i === 0) {
-      const height = total / i;
-      if ((height - 2) * (i - 2) === yellow) {
-        return [height, i]
-      }
+  for (let i = 3; i <= (brown - 2) / 2; i++) {
+    if (i * (brown / 2 - i + 2) === total && (i - 2) * (brown / 2 - i) === yellow) {
+      if (answer[0] < i) answer = [i, brown / 2 - i + 2];
+            
     }
   }
+  return answer
 };
