14717704507dd183286af5dcd3f37d6b0fcf8592
Gotkwondo <dhwngus99@gmail.com>
2024-04-03 13:59:06 +0900

    ✨ feat(2 level): 점프와 순간 이동 : 구현, 수학, 반복문(while)
    가야하는 길이를 2로 나눈 나머지를 기준으로 점할지 이동할지 결정

--- Programmers/Level_2/점프와 순간 이동.js
+++ Programmers/Level_2/점프와 순간 이동.js
@@ -1,20 +1,39 @@
+// const solution = (n) => {
+//   let answer = 1;
+//   if (n > 1) {
+//     while (true) {
+//       if (n === 1) {
+//         return answer;
+//       }
+//       else {
+//         if (n % 2 === 0) {
+//           n = n / 2;
+//         }
+//         else {
+//           n -= 1;
+//           answer += 1;
+//         }
+//       }
+//     }
+//   }
+//   return answer;
+// }
+
 const solution = (n) => {
-  let answer = 1;
-  if (n > 1) {
-    while (true) {
-      if (n === 1) {
-        return answer;
+  let answer = 0;
+  if (n === 1) return 1;
+  while (true) {
+    if (n === 1) {
+      return answer + 1;
+    }
+    else {
+      if (n % 2 === 0) {
+        n = n / 2;
       }
       else {
-        if (n % 2 === 0) {
-          n = n / 2;
-        }
-        else {
-          n -= 1;
-          answer += 1;
-        }
+        n -= 1;
+        answer += 1;
       }
     }
   }
-  return answer;
-}
+};
