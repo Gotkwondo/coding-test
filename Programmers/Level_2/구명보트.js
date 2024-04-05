f6af27302066b20588f2a4df67cdb31bce1c9fd8
Gotkwondo <dhwngus99@gmail.com>
2024-04-05 11:19:31 +0900

    ✨ feat(2 level): 구명보트 : 그리디, 시간복잡도(반복문 초기에 pop()으로 큰 수를 뽑아 시간복잡도 최적화)

--- Programmers/Level_2/구명보트.js
+++ Programmers/Level_2/구명보트.js
@@ -1,26 +1,43 @@
+// const solution = (people, limit) => {
+//   let answer = 0;
+//   people.sort((a, b) => a - b);
+
+//   let fmen = 0;
+//   let lmen = people.length - 1;
+
+//   while (fmen <= lmen) {
+//     //  혼자 남았을 때
+//     if (fmen === lmen) {
+//       answer += 1;
+//       break;
+//     }
+//     else if (people[fmen] + people[lmen] <= limit) {
+//       fmen += 1;
+//       lmen -= 1;
+//       answer += 1;
+//     }
+//     else {
+//       lmen -= 1;
+//       answer += 1;
+//     }
+//   }
+
+//   return answer;
+// }
+
 const solution = (people, limit) => {
+  let arr = people.sort((a, b) => a - b);
   let answer = 0;
-  people.sort((a, b) => a - b);
-
-  let fmen = 0;
-  let lmen = people.length - 1;
-
-  while (fmen <= lmen) {
-    //  혼자 남았을 때
-    if (fmen === lmen) {
-      answer += 1;
-      break;
-    }
-    else if (people[fmen] + people[lmen] <= limit) {
-      fmen += 1;
-      lmen -= 1;
+    
+  while (arr.length) {
+    const big = arr.pop();
+    if (arr[0] + big <= limit) {
+      arr.shift();
       answer += 1;
     }
     else {
-      lmen -= 1;
       answer += 1;
     }
   }
-
   return answer;
-}
+};
