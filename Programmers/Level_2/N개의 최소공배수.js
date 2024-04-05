ceeba9c82d593f43bc3b93347ada5ffed32d9ba4
Gotkwondo <dhwngus99@gmail.com>
2024-04-05 20:18:28 +0900

    ✨ feat(2 level): n개의 최소공배수 : 수학, 구현, 최대공약수, 최소공배수

--- Programmers/Level_2/N개의 최소공배수.js
+++ Programmers/Level_2/N개의 최소공배수.js
@@ -1,7 +1,19 @@
-const solution = (arr) => {
-  return arr.reduce((a, b) => {
-    return (a * b) / getGcd(a, b)
-  }, 1)
-};
+// const solution = (arr) => {
+//   return arr.reduce((a, b) => {
+//     return (a * b) / getGcd(a, b)
+//   }, 1)
+// };
 
-const getGcd = (a, b) => a % b === 0 ? b : getGcd(b, a % b)
+// const getGcd = (a, b) => a % b === 0 ? b : getGcd(b, a % b)
+
+const solution = (arr) => {
+  // 최대 공약수 구하는 공식
+  const getGCD = (a, b) => {
+    return a % b ? getGCD(b, a % b) : b;
+  }
+    
+  return arr.reduce((acc, cur) => {
+    // 최소 공배수는 두 수를 곱한뒤 두 수의 최대 공약수로 나누면 구해진다.
+    return acc * cur / getGCD(acc, cur);
+  }, 1);
+};
