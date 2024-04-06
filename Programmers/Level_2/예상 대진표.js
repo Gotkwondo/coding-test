7d10ddfb1178592bc5216e005af332d7c0b9edfd
Gotkwondo <dhwngus99@gmail.com>
2024-04-06 15:51:49 +0900

    ✨ feat(2 level): 예상 대진표 : 수학, 구현, 대진표

--- Programmers/Level_2/예상 대진표.js
+++ Programmers/Level_2/예상 대진표.js
@@ -1,10 +1,22 @@
-const solution = (n, a, b) => {
-  let cnt = 0;
+// const solution = (n, a, b) => {
+//   let cnt = 0;
     
-  while (a !== b) {
-    a = Math.ceil(a / 2);
-    b = Math.ceil(b / 2);
-    cnt++;
+//   while (a !== b) {
+//     a = Math.ceil(a / 2);
+//     b = Math.ceil(b / 2);
+//     cnt++;
+//   }
+//   return cnt
+// }
+
+const solution = (n, a, b) => {
+  let [A, B] = [a, b];
+  let answer = 0;
+  while (true) {
+    if (A === B) break;
+    A = Math.ceil(A / 2);
+    B = Math.ceil(B / 2);
+    answer += 1;
   }
-  return cnt
-}
+  return answer;
+};
