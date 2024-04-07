f8a02907a1ac10b11c9126899f5b0b49a8621d2a
Gotkwondo <dhwngus99@gmail.com>
2024-04-07 19:11:50 +0900

    ✨ feat(2 level): 멀리 뛰기 : DP, 동적 프로그래밍

--- Programmers/Level_2/멀리 뛰기.js
+++ Programmers/Level_2/멀리 뛰기.js
@@ -1,12 +1,26 @@
+// const solution = (n) => {
+//   const dp = (n) => {
+//     const dp = new Array(n + 1).fill(0);
+//     dp[0] = 1;
+//     dp[1] = 1;
+//     for (let i = 2; i <= n; i++) {
+//       dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+//     }
+//     return dp[n]
+//   }
+//   return dp(n);
+// }
+
 const solution = (n) => {
-  const dp = (n) => {
-    const dp = new Array(n + 1).fill(0);
-    dp[0] = 1;
-    dp[1] = 1;
-    for (let i = 2; i <= n; i++) {
-      dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+  let arr = Array(n + 1).fill(0);
+  arr[0] = 1;
+  arr[1] = 1;
+    
+  const dp = (num) => {
+    if (!arr[num]) {
+      arr[num] = dp(num - 1) + dp(num - 2);
     }
-    return dp[n]
+    return arr[num] % 1234567;
   }
   return dp(n);
-}
+};
