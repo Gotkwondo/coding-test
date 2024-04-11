04b7148d3bea499c1de3ef3f7083c2c586f04fbf
오주현 <dhwngus99@gmail.com>
2024-04-11 20:29:01 +0900

    ✨ feat(2 level): 의상 : 구현, 수학 (각 종류의 개수+1 만큼씩 곱하면 경우의 수가 나오고, 맨 마지막에 -1은 아무것도 안입은 경우)

--- Programmers/Level_2/의상.js
+++ Programmers/Level_2/의상.js
@@ -1,8 +1,19 @@
 const solution = (clothes) => {
-  return Object.values(
-    clothes.reduce((obj, type) => {
-      obj[type[1]] = obj[type[1]] ? obj[type[1]] + 1 : 1;
-      return obj;
-    }, {})
-  ).reduce((a, b) => a * (b + 1), 1) - 1;
+  const obj = {};
+  let answer = 1;
+    
+  clothes.forEach(e => obj[e[1]] = (obj[e[1]] ? obj[e[1]] : 0) + 1);
+  for (const val of Object.values(obj)) {
+    answer *= val + 1;
+  }
+  return answer - 1;
 };
+
+// const solution = (clothes) => {
+//   return Object.values(
+//     clothes.reduce((obj, type) => {
+//       obj[type[1]] = obj[type[1]] ? obj[type[1]] + 1 : 1;
+//       return obj;
+//     }, {})
+//   ).reduce((a, b) => a * (b + 1), 1) - 1;
+// };
