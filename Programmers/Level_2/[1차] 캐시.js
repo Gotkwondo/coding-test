4aa0ba1c2b13b5bf408a3085ef0d1a820a988a5a
오주현 <dhwngus99@gmail.com>
2024-04-11 19:44:13 +0900

    ✨ feat(2 level): [카카오] 캐시 : FIFO, 구현 (큐 문제와 비슷한 LRU알고리즘을 이용한 중복 확인 문제)

--- Programmers/Level_2/[1차] 캐시.js
+++ Programmers/Level_2/[1차] 캐시.js
@@ -1,30 +1,58 @@
 const solution = (cacheSize, cities) => {
-  let cache = [];
   let runtime = 0;
-  let smallCities = cities.map(e => e.toLowerCase())
-  // 캐시의 크기가 0이면 들어가서 hit할 수 있는 경우가 없어지기에 miss가 나는 경우
-  if (cacheSize === 0) {
-    return cities.length * 5;
-  }
-  smallCities.forEach((e, i) => {
-    const idx = cache.indexOf(e);
-    // 있는 경우
-    if (cache.length && cache.includes(e)) {
+  let Cache = [];
+  const low = cities.map(e => e.toLowerCase());
+    
+  if (cacheSize === 0) return cities.length * 5;
+    
+  for (let i = 0; i < low.length; i++) {
+    const idx = Cache.indexOf(low[i]);
+    if (idx !== -1) {
       runtime += 1;
-      cache.splice(idx, 1)
-      cache.push(e);
+      Cache.splice(idx, 1);
+      Cache.push(low[i])
     }
-    // 없는 경우
     else {
-      if (cache.length < cacheSize) {
-        cache.push(e);
+      runtime += 5;
+      if (Cache.length === 0 || Cache.length < cacheSize) {
+        Cache.push(low[i]);
       }
       else {
-        cache.shift();
-        cache.push(e)
+        Cache.shift();
+        Cache.push(low[i])
       }
-      runtime += 5;
     }
-  })
+  }
   return runtime;
 };
+
+// const solution = (cacheSize, cities) => {
+//   let cache = [];
+//   let runtime = 0;
+//   let smallCities = cities.map(e => e.toLowerCase())
+//   // 캐시의 크기가 0이면 들어가서 hit할 수 있는 경우가 없어지기에 miss가 나는 경우
+//   if (cacheSize === 0) {
+//     return cities.length * 5;
+//   }
+//   smallCities.forEach((e, i) => {
+//     const idx = cache.indexOf(e);
+//     // 있는 경우
+//     if (cache.length && cache.includes(e)) {
+//       runtime += 1;
+//       cache.splice(idx, 1)
+//       cache.push(e);
+//     }
+//     // 없는 경우
+//     else {
+//       if (cache.length < cacheSize) {
+//         cache.push(e);
+//       }
+//       else {
+//         cache.shift();
+//         cache.push(e)
+//       }
+//       runtime += 5;
+//     }
+//   })
+//   return runtime;
+// };
