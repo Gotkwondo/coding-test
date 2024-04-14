ba1782e8ad8947c173cbdb816ad60bc05c601a55
Gotkwondo <dhwngus99@gmail.com>
2024-04-14 18:22:35 +0900

    ✨ feat(2 level): 뉴스 클러스터링 : 중복 확인, 교집합, 합집합, 구현 (Set 자료구조를 이용해 요소의 개수를 파악후 교집합, 합집합을 구한다.)

--- Programmers/Level_2/[1차] 뉴스 클러스터링.js
+++ Programmers/Level_2/[1차] 뉴스 클러스터링.js
@@ -1,30 +1,58 @@
 const solution = (str1, str2) => {
-  const str1R = makeArr(str1.toLowerCase());
-  const str2R = makeArr(str2.toLowerCase());
-    
-  const set = [...new Set([...str1R, ...str2R])];
-  let union = 0;
-  let intersection = 0;
-    
-  set.forEach((e) => {
-    const have1 = str1R.filter((el) => el === e).length;
-    const have2 = str2R.filter((el) => el === e).length;
-    union += Math.max(have1, have2);
-    intersection += Math.min(have1, have2);
-  })
-  if (union === intersection) return 65536;
-  else {
-    return Math.floor((intersection / union) * 65536);
-  }
-};
-
-const makeArr = (str) => {
-  let arr = [];
-  for (let i = 0; i < str.length; i++) {
-    const s = str.substr(i, 2);
-    if (s.match(/[a-z]{2}/, 'g')) {
-      arr.push(s);
+    const change = (str) => {
+        let arr = [];
+        for(let i = 0; i < str.length - 1; i++){
+            if(str[i].match(/[a-z]/) && str[i+1].match(/[a-z]/)) arr.push(str[i]+str[i+1]);
+        }
+        return arr;
     }
-  }
-  return arr;
-};
+    let st1 = change(str1.toLowerCase().trim());
+    let st2 = change(str2.toLowerCase().trim());
+    
+    const set = new Set([...st1, ...st2]);
+    let union = 0;
+    let inter = 0;
+    
+    set.forEach(e => {
+        const has1 = st1.filter(el => el === e).length;
+        const has2 = st2.filter(el => el === e).length;
+        
+        union += Math.max(has1, has2);
+        inter += Math.min(has1, has2);
+    })
+    
+    if(union === inter) return 65536;
+    
+    return Math.floor((inter / union) * 65536);
+}
+
+// const solution = (str1, str2) => {
+//   const str1R = makeArr(str1.toLowerCase());
+//   const str2R = makeArr(str2.toLowerCase());
+    
+//   const set = [...new Set([...str1R, ...str2R])];
+//   let union = 0;
+//   let intersection = 0;
+    
+//   set.forEach((e) => {
+//     const have1 = str1R.filter((el) => el === e).length;
+//     const have2 = str2R.filter((el) => el === e).length;
+//     union += Math.max(have1, have2);
+//     intersection += Math.min(have1, have2);
+//   })
+//   if (union === intersection) return 65536;
+//   else {
+//     return Math.floor((intersection / union) * 65536);
+//   }
+// };
+
+// const makeArr = (str) => {
+//   let arr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     const s = str.substr(i, 2);
+//     if (s.match(/[a-z]{2}/, 'g')) {
+//       arr.push(s);
+//     }
+//   }
+//   return arr;
+// };
