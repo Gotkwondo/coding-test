39546acf006bca0507fdfab5a43a26334bd093d3
Gotkwondo <dhwngus99@gmail.com>
2024-04-14 17:12:13 +0900

    ✨ feat(2 level): 프로세스 : 큐, 검증(큐 형태의 알고리즘으로 맨 앞의 인덱스 값과 중요도를 비교해 검증하기)

--- Programmers/Level_2/프로세스.js
+++ Programmers/Level_2/프로세스.js
@@ -1,15 +1,40 @@
 const solution = (priorities, location) => {
-  let priArr = priorities.map((e, i) => [i, e]);
-  let arr = [];
-    
-  while (priArr.length > 0) {
-    const prevV = priArr.shift();
-    if (priArr.find(e => e[1] > prevV[1])) {
-      priArr.push(prevV);
+  let arr = priorities.map((e, i) => {
+    return {
+      idx: i,
+      el: e
+    }
+  });
+  let answer = 1;
+
+  while (arr.length) {
+    const val = arr.shift();
+    if (arr.some(e => e.el > val.el)) {
+      arr.push(val);
     }
     else {
-      arr.push(prevV)
+      if (val.idx === location) {
+        return answer;
+      }
+      else {
+        answer++;
+      }
     }
   }
-  return arr.findIndex((e) => e[0] === location) + 1;
-}
+};
+
+// const solution = (priorities, location) => {
+//   let priArr = priorities.map((e, i) => [i, e]);
+//   let arr = [];
+    
+//   while (priArr.length > 0) {
+//     const prevV = priArr.shift();
+//     if (priArr.find(e => e[1] > prevV[1])) {
+//       priArr.push(prevV);
+//     }
+//     else {
+//       arr.push(prevV)
+//     }
+//   }
+//   return arr.findIndex((e) => e[0] === location) + 1;
+// }
