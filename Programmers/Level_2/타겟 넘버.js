12362cdbdd58ff56e1a9fcf69296527269b940a4
오주현 <dhwngus99@gmail.com>
2024-04-19 03:53:11 +0900

    ✨ feat(2 level): 타겟 넘버 : DFS

--- Programmers/Level_2/타겟 넘버.js
+++ Programmers/Level_2/타겟 넘버.js
@@ -1,18 +1,38 @@
 const solution = (numbers, target) => {
   let answer = 0;
     
-  const dfs = (count, sum) => {
-    if (count < numbers.length) {
-      dfs(count + 1, sum + numbers[count]);
-      dfs(count + 1, sum - numbers[count]);
+  const dfs = (cnt, val) => {
+    if (cnt < numbers.length) {
+      dfs(cnt + 1, val + numbers[cnt]);
+      dfs(cnt + 1, val - numbers[cnt]);
     }
     else {
-      if (sum === target) {
-        answer++;
+      if (val === target) {
+        answer += 1;
       }
       return;
     }
   }
   dfs(0, 0);
+    
   return answer;
-}
+};
+
+// const solution = (numbers, target) => {
+//   let answer = 0;
+    
+//   const dfs = (count, sum) => {
+//     if (count < numbers.length) {
+//       dfs(count + 1, sum + numbers[count]);
+//       dfs(count + 1, sum - numbers[count]);
+//     }
+//     else {
+//       if (sum === target) {
+//         answer++;
+//       }
+//       return;
+//     }
+//   }
+//   dfs(0, 0);
+//   return answer;
+// }
