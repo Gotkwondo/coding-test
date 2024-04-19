7b29c96a7e1524ff65d55c3cbf1ac87e133e03c9
오주현 <dhwngus99@gmail.com>
2024-04-19 22:27:35 +0900

    ✨ feat(2 level): k진수에서 소수 개수 구하기: 구현, 소수 판별(소수 판별식을 이용한 문제 풀이 필수)

--- Programmers/Level_2/k진수에서 소수 개수 구하기.js
+++ Programmers/Level_2/k진수에서 소수 개수 구하기.js
@@ -1,11 +1,28 @@
 const solution = (n, k) => {
-  return n.toString(k).split('0').filter((e) => isPrime(+e)).length;
+    let answer = 0;
+    const num = n.toString(k).split('0').filter(e => e !== '1' && e !== '');
+    num.forEach(e => {
+        if(isPrime(e)) answer += 1;
+    });
+    return answer;
 }
 
-const isPrime = (num) => {
-  if (!num || num < 2) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
+const isPrime = (n) => {
+  if (+n === 2) return true;
+  for (let i = 2; i <= Math.sqrt(+n); i++) {
+    if (+n % i === 0) return false;
   }
-  return true
-}
+  return true;
+};
+
+// const solution = (n, k) => {
+//   return n.toString(k).split('0').filter((e) => isPrime(+e)).length;
+// }
+
+// const isPrime = (num) => {
+//   if (!num || num < 2) return false;
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) return false;
+//   }
+//   return true
+// }
