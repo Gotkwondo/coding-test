1756d63b6d53b6693ce7816eba3d3b9e7a7c5f8b
오주현 <dhwngus99@gmail.com>
2024-04-20 23:51:04 +0900

    ✨ feat(2 level): 모음사전 : 완전탐색, DFS, 구현 (DFS를 이용해 모든 단어의 수를 구한 다음 정렬을 이용해 순서대로 정렬 후 indexOf를 이용해 답 도출)

--- Programmers/Level_2/모음사전.js
+++ Programmers/Level_2/모음사전.js
@@ -1,17 +1,38 @@
 const solution = (word) => {
-  let arr = [];
-  const dfs = (str, length) => {
-    const vow = [...'AEIOU'];
-    if (str.length === length) {
-      arr.push(str);
+  let answer = [];
+  const v = ['A', 'E', 'I', 'O', 'U'];
+
+  const dfs = (n, l) => {
+    if (n.length === l) {
+      answer.push(n);
       return;
     }
-    vow.forEach((e) => {
-      dfs(str + e, length);
-    })
+    else {
+      v.forEach(e => {
+        dfs(n + e, l);
+      })
+    }
   }
   for (let i = 1; i <= 5; i++) {
     dfs('', i);
   }
-  return arr.sort().indexOf(word) + 1;
-}
+  return answer.sort().indexOf(word) + 1;
+};
+
+// const solution = (word) => {
+//   let arr = [];
+//   const dfs = (str, length) => {
+//     const vow = [...'AEIOU'];
+//     if (str.length === length) {
+//       arr.push(str);
+//       return;
+//     }
+//     vow.forEach((e) => {
+//       dfs(str + e, length);
+//     })
+//   }
+//   for (let i = 1; i <= 5; i++) {
+//     dfs('', i);
+//   }
+//   return arr.sort().indexOf(word) + 1;
+// }
