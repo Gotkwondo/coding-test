e8ae02b44324bc8091c06138720b820f6f9ca762
오주현 <dhwngus99@gmail.com>
2024-04-30 21:22:55 +0900

    ✨ feat(silver 1): 스타트와 링크: 완전탐색, DFS, 백트래킹

--- BeakJoon/14889_스타트와 링크/14889.js
+++ BeakJoon/14889_스타트와 링크/14889.js
@@ -14,7 +14,7 @@ const dfs = (cnt, join) => {
     let aScore = 0;
     let bScore = 0;
 
-    for (let i = 0; i < N; i++){
+    for (let i = 0; i < N; i++) {
       if (visited[i]) {
         aTeam.push(i);
       }
@@ -22,8 +22,8 @@ const dfs = (cnt, join) => {
         bTeam.push(i);
       }
     }
-    for (let i = 0; i < N / 2; i++){
-      for (let j = i + 1; j < N / 2; j++){
+    for (let i = 0; i < N / 2; i++) {
+      for (let j = i + 1; j < N / 2; j++) {
         aScore += arr[aTeam[i]][aTeam[j]] + arr[aTeam[j]][aTeam[i]];
         bScore += arr[bTeam[i]][bTeam[j]] + arr[bTeam[j]][bTeam[i]];
       }
@@ -32,14 +32,14 @@ const dfs = (cnt, join) => {
     return;
   }
   
-  for (let i = join; i < N; i++){
+  for (let i = join; i < N; i++) {
     if (!visited[i]) {
       visited[i] = true;
       dfs(cnt + 1, i);
       visited[i] = false;
     }
   }
-}
+};
 dfs(0,0)
 
 console.log(answer)
