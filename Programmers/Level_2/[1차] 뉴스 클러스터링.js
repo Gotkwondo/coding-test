c95bff759b88650a9d9d78e8143fce2cbfbf113e
오주현 <dhwngus99@gmail.com>
2024-09-27 21:24:46 +0900

    ✨ feat(level 2): 뉴스클러스터링: 문자열, 중복, 교집합, 합집합, 집합, Set객체

--- Programmers/Level_2/[1차] 뉴스 클러스터링.js
+++ Programmers/Level_2/[1차] 뉴스 클러스터링.js
@@ -1,30 +1,58 @@
+const makeArr = (arr) => {
+  const map = new Map();
+  for (let i = 1; i < arr.length; i++) {
+    const ja = arr[i - 1] + arr[i];
+    if (!/[^a-z]/g.test(ja)) map.has(ja) ? map.set(ja, map.get(ja) + 1) : map.set(ja, 1);
+  }
+  return map;
+};
+
 const solution = (str1, str2) => {
-    const change = (str) => {
-        let arr = [];
-        for(let i = 0; i < str.length - 1; i++){
-            if(str[i].match(/[a-z]/) && str[i+1].match(/[a-z]/)) arr.push(str[i]+str[i+1]);
-        }
-        return arr;
-    }
-    let st1 = change(str1.toLowerCase().trim());
-    let st2 = change(str2.toLowerCase().trim());
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
     
-    const set = new Set([...st1, ...st2]);
-    let union = 0;
-    let inter = 0;
+  const map1 = makeArr(str1);
+  const map2 = makeArr(str2);
     
-    set.forEach(e => {
-        const has1 = st1.filter(el => el === e).length;
-        const has2 = st2.filter(el => el === e).length;
+  const cnt = [...new Set([...map1.keys(), ...map2.keys()])];
+  let union = 0;
+  let inter = 0;
+    
+  cnt.forEach(e => {
+    union += Math.max(map1.has(e) ? map1.get(e) : 0, map2.has(e) ? map2.get(e) : 0);
+    inter += Math.min(map1.has(e) ? map1.get(e) : 0, map2.has(e) ? map2.get(e) : 0);
+  })
+    
+  return isNaN(inter / union) ? 65536 : Math.floor(inter / union * 65536);
+};
+
+// const solution = (str1, str2) => {
+//     const change = (str) => {
+//         let arr = [];
+//         for(let i = 0; i < str.length - 1; i++){
+//             if(str[i].match(/[a-z]/) && str[i+1].match(/[a-z]/)) arr.push(str[i]+str[i+1]);
+//         }
+//         return arr;
+//     }
+//     let st1 = change(str1.toLowerCase().trim());
+//     let st2 = change(str2.toLowerCase().trim());
+    
+//     const set = new Set([...st1, ...st2]);
+//     let union = 0;
+//     let inter = 0;
+    
+//     set.forEach(e => {
+//         const has1 = st1.filter(el => el === e).length;
+//         const has2 = st2.filter(el => el === e).length;
         
-        union += Math.max(has1, has2);
-        inter += Math.min(has1, has2);
-    })
+//         union += Math.max(has1, has2);
+//         inter += Math.min(has1, has2);
+//     })
     
-    if(union === inter) return 65536;
+//     if(union === inter) return 65536;
     
-    return Math.floor((inter / union) * 65536);
-}
+//     return Math.floor((inter / union) * 65536);
+// }
 
 // const solution = (str1, str2) => {
 //   const str1R = makeArr(str1.toLowerCase());
