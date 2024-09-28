c4135e986b4d0aa02c7c3d261aeb2a8c1aa8c229
Gotkwondo <dhwngus99@gmail.com>
2024-09-28 15:16:18 +0900

    ✨ feat(level 2): n 진수 게임: 구현, 문자열

--- Programmers/Level_2/[3차] n진수 게임.js
+++ Programmers/Level_2/[3차] n진수 게임.js
@@ -1,12 +1,26 @@
 const solution = (n, t, m, p) => {
-  let answer = '';
-  let arr = [];
-  for (let i = 0; i < t * m; i++) {
-    arr.push(i.toString(n));
-  }
-  const str = arr.join('');
-  for (let i = p - 1; i < str.length; i += m) {
-    answer += str[i];
-    if (answer.length === t) return answer.toUpperCase();
-  }
-}
+  const st = Array.from({ length: t * m }, (_, i) => i.toString(n)).join('').toUpperCase().split('');
+  p -= 1;
+  const test = st.reduce((acc, cur, idx) => {
+    if (idx === p) {
+      p += m;
+      return acc + cur;
+    }
+    return acc;
+  }, '');
+    
+  return test.slice(0, t);
+};
+
+// const solution = (n, t, m, p) => {
+//   let answer = '';
+//   let arr = [];
+//   for (let i = 0; i < t * m; i++) {
+//     arr.push(i.toString(n));
+//   }
+//   const str = arr.join('');
+//   for (let i = p - 1; i < str.length; i += m) {
+//     answer += str[i];
+//     if (answer.length === t) return answer.toUpperCase();
+//   }
+// }
