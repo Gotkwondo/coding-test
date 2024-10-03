5397174343881953f1eba8f9c066372e840d8b50
Gotkwondo <dhwngus99@gmail.com>
2024-10-03 11:10:17 +0900

    ✨ feat(level 1): 크레인 인형뽑기: 구현

--- Programmers/Level_1/크레인 인형뽑기 게임.js
+++ Programmers/Level_1/크레인 인형뽑기 게임.js
@@ -1,22 +1,54 @@
 const solution = (board, moves) => {
   const stack = [];
-  let cnt = 0;
-
-  for (let i = 0; i < moves.length; i++) {
-    let y = 0;
-        
-    while ((board[y][moves[i] - 1] === 0) && y < board.length) {
-      y += 1;
-      if (y === board.length) break;
+  let answer = 0;
+    
+  const getIdx = (board, line) => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][line] > 0) {
+        return i;
+      }
     }
-    if (y === board.length) continue;
-    const sl = board[y][moves[i] - 1];
-    board[y][moves[i] - 1] = 0;
-    stack.push(sl);
-    if (stack.length > 1 && stack[stack.length - 1] === stack[stack.length - 2]) {
-      stack.splice(-2, 2);
-      cnt += 2;
+    return -1;
+  }
+    
+  for (let i = 0; i < moves.length; i++) {
+    const line = moves[i] - 1;
+    const arr = board[line];
+    const idx = getIdx(board, line);
+    if (idx === -1) continue;
+    else {
+      stack.push(board[idx][line]);
+      board[idx][line] = 0;
+    }
+        
+    if (stack.length >= 2 && stack[stack.length - 1] === stack[stack.length - 2]) {
+      stack.pop();
+      stack.pop();
+      answer += 2;
     }
   }
-  return cnt;
+  return answer;
 }
+
+// const solution = (board, moves) => {
+//   const stack = [];
+//   let cnt = 0;
+
+//   for (let i = 0; i < moves.length; i++) {
+//     let y = 0;
+        
+//     while ((board[y][moves[i] - 1] === 0) && y < board.length) {
+//       y += 1;
+//       if (y === board.length) break;
+//     }
+//     if (y === board.length) continue;
+//     const sl = board[y][moves[i] - 1];
+//     board[y][moves[i] - 1] = 0;
+//     stack.push(sl);
+//     if (stack.length > 1 && stack[stack.length - 1] === stack[stack.length - 2]) {
+//       stack.splice(-2, 2);
+//       cnt += 2;
+//     }
+//   }
+//   return cnt;
+// }
