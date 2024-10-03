cd74ed9b2da6ad6020240a37336b7b71fcca6434
오주현 <dhwngus99@gmail.com>
2024-10-03 16:39:12 +0900

    ✨ feat(level 3): 징검다리 건너기: 투포인터, 구현, 관점을 다르게 보기

--- Programmers/Level_3/보석 쇼핑.js
+++ Programmers/Level_3/보석 쇼핑.js
@@ -1,26 +1,47 @@
-function solution(gems) {
-  const size = new Set(gems).size;
-  const map = new Map();
-  let answer = [1, gems.length];
+function solution(stones, k) {
   let lp = 0;
-  let rp = 0;
+  let rp = 200000000;
 
-  map.set(gems[0], 1);
-  while (rp < gems.length) {
-    if (map.size === size) {
-      if (answer[1] - answer[0] > rp - lp) answer = [lp + 1, rp + 1];
-      map.set(gems[lp], map.get(gems[lp]) - 1);
-      if (map.get(gems[lp]) === 0) map.delete(gems[lp]);
-      lp++
-    }
-    else {
-      rp++;
-      map.set(gems[rp], map.has(gems[rp]) ? map.get(gems[rp]) + 1 : 1);
-    }
+  while (lp <= rp) {
+    const mid = Math.floor((lp + rp) / 2);
+    let cnt = 0;
+    let flag = false;
 
+    for (let i = 0; i < stones.length; i++) {
+      cnt = stones[i] - mid > 0 ? 0 : cnt + 1;
+      if (cnt === k) {
+        flag = true;
+        break;
+      }
+    }
+    flag ? rp = mid - 1 : lp = mid + 1;
   }
-  return answer;
+  return lp;
 }
+
+// function solution(gems) {
+//   const size = new Set(gems).size;
+//   const map = new Map();
+//   let answer = [1, gems.length];
+//   let lp = 0;
+//   let rp = 0;
+
+//   map.set(gems[0], 1);
+//   while (rp < gems.length) {
+//     if (map.size === size) {
+//       if (answer[1] - answer[0] > rp - lp) answer = [lp + 1, rp + 1];
+//       map.set(gems[lp], map.get(gems[lp]) - 1);
+//       if (map.get(gems[lp]) === 0) map.delete(gems[lp]);
+//       lp++
+//     }
+//     else {
+//       rp++;
+//       map.set(gems[rp], map.has(gems[rp]) ? map.get(gems[rp]) + 1 : 1);
+//     }
+
+//   }
+//   return answer;
+// }
 
 // function solution1(gems) {
 //   const size = new Set(gems).size;
