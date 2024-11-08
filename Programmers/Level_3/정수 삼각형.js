eb1f10441bf6069252254da4a112cbfa8f8e0f7b
Gotkwondo <dhwngus99@gmail.com>
2024-11-08 13:39:25 +0900

    ✨ feat(level 3): dP, 탑 다운

--- Programmers/Level_3/정수 삼각형.js
+++ Programmers/Level_3/정수 삼각형.js
@@ -1,26 +1,43 @@
-// 탑다운 방식
 const solution = (triangle) => {
-  for (let i = 1; i < triangle.length; i++) {
+  const n = triangle.length;
+    
+  for (let i = 1; i < n; i++) {
     for (let j = 0; j < triangle[i].length; j++) {
-      const left = triangle[i - 1][j - 1] ?? 0;
-      const right = triangle[i - 1][j] ?? 0;
-      if (left >= right) {
-        triangle[i][j] += left;
+      if (j === 0) {
+        triangle[i][j] += triangle[i - 1][j];
       }
-      else {
-        triangle[i][j] += right;
+      else if (j === triangle[i].length - 1) {
+        triangle[i][j] += triangle[i - 1][j - 1];
       }
+      else triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
     }
   }
-  return Math.max(...triangle[triangle.length - 1]);
-};
+  return Math.max(...triangle[n - 1]);
+}
 
-// 바텀업 방식
-const solution = (triangle) => {
-  for (let i = triangle.length - 2; i >= 0; i--) {
-    for (let j = 0; j < triangle[i].length; j++) {
-      triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
-    }
-  }
-  return triangle[0][0];
-};
+// // 탑다운 방식
+// const solution = (triangle) => {
+//   for (let i = 1; i < triangle.length; i++) {
+//     for (let j = 0; j < triangle[i].length; j++) {
+//       const left = triangle[i - 1][j - 1] ?? 0;
+//       const right = triangle[i - 1][j] ?? 0;
+//       if (left >= right) {
+//         triangle[i][j] += left;
+//       }
+//       else {
+//         triangle[i][j] += right;
+//       }
+//     }
+//   }
+//   return Math.max(...triangle[triangle.length - 1]);
+// };
+
+// // 바텀업 방식
+// const solution = (triangle) => {
+//   for (let i = triangle.length - 2; i >= 0; i--) {
+//     for (let j = 0; j < triangle[i].length; j++) {
+//       triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+//     }
+//   }
+//   return triangle[0][0];
+// };
