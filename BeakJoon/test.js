758567c4ae1b4367b4ad30f38116168d16fe2c33
Gotkwondo <dhwngus99@gmail.com>
2024-11-12 17:27:40 +0900

    ✨ feat(level 3): dFS, 그래프 탐색

--- BeakJoon/test.js
+++ BeakJoon/test.js
@@ -3,29 +3,17 @@ const input = require('fs')
   .toString().trim().split('\n');
 
 const n = +input.shift();
-const linked = +input.shift();
 const arr = input.map(e => e.split(' ').map(Number));
-const map = {};
-let answer = 0;
-const check = Array.from({ length: n }, () => false);
 
-for (let i = 0; i < arr.length; i++){
-  const [st, ed] = arr[i];
-  map[st] ? map[st].push(ed) : map[st] = [ed];
-  map[ed] ? map[ed].push(st) : map[ed] = [st];
+const rMap = Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => 0));
+const lMap = Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => 0));
+
+const check1 = (x, y) => {
+  
 }
 
-const dfs = (target) => {
-  if (check[target - 1] || !map[target]) {
-    return;
-  }
-  const list = map[target];
-  check[target - 1] = true;
-  for (let i = 0; i < list.length; i++){
-    dfs(list[i]);
-  }
-}
+arr.forEach(([t, x, y]) => {
 
-dfs(1)
-answer = check.filter(e => e === true).length
-console.log(answer > 0 ? answer - 1 : 0);
+})
+
+console.log(rMap, lMap)
