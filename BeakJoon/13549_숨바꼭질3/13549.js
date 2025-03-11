169a77e17afb589df982d388e8454f25c8f1eec7
오주현 <dhwngus99@gmail.com>
2025-03-11 13:59:25 +0900

    ✨ feat(gold5): 숨바꼭질3: BFS, 구현

--- BeakJoon/13549_숨바꼭질3/13549.js
+++ BeakJoon/13549_숨바꼭질3/13549.js
@@ -1,8 +1,12 @@
-const input = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
-  .toString().trim().split('\n');
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/example.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
 
-const [st, ed] = input[0].split(' ').map(Number);
+const [st, ed] = input[0].split(" ").map(Number);
 let que = [[st, 0]];
 let visited = Array.from({ length: 100001 }, () => false);
 visited[st] = true;
@@ -14,8 +18,8 @@ while (que.length) {
     console.log(t);
     break;
   }
-  
-  for (let d of [cur * 2, cur - 1 , cur + 1]) {
+
+  for (let d of [cur * 2, cur - 1, cur + 1]) {
     if (!visited[d] && d >= 0 && d < 100001) {
       visited[d] = true;
       if (d === cur * 2) que.unshift([d, t]);
