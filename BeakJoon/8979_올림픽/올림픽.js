8ac6233f6ad592f7c61b532b0c3928fa32dfb65f
오주현 <dhwngus99@gmail.com>
2025-03-15 23:22:51 +0900

    ✨ feat(silver 5): 올림픽: 정렬, 구현, 중복 개수 처리

---
+++ BeakJoon/8979_올림픽/올림픽.js
@@ -0,0 +1,40 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/example.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, k] = input.shift().split(" ").map(Number);
+const arr = input.map((e) => [...e.split(" ").map(Number), 0]);
+let same = 1;
+
+arr.sort((a, b) => {
+  if (b[1] > a[1]) return 1;
+  else if (b[1] === a[1]) {
+    if (b[2] > a[2]) return 1;
+    else if (b[2] === a[2]) {
+      if (b[3] > a[3]) return 1;
+      else if (b[3] === a[3]) return 0;
+      else return -1;
+    } else return -1;
+  } else return -1;
+});
+
+arr[0][4] = 1;
+for (let i = 1; i < n; i++) {
+  if (
+    arr[i - 1][1] === arr[i][1] &&
+    arr[i - 1][2] === arr[i][2] &&
+    arr[i - 1][3] === arr[i][3]
+  ) {
+    arr[i][4] = arr[i - 1][4];
+    same++;
+  } else {
+    arr[i][4] = arr[i - 1][4] + same;
+    same = 1;
+  }
+}
+
+console.log(arr.find((e) => e[0] === k)[4]);
--- BeakJoon/test.js
+++ BeakJoon/test.js
@@ -2,24 +2,33 @@ const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
   .toString().trim().split('\n');
 
-const [st, ed] = input[0].split(' ').map(Number);
-let que = [[st, 0]];
-let visited = Array.from({ length: 100001 }, () => false);
-visited[st] = true;
+const [n, k] = input.shift().split(' ').map(Number);
+const arr = input.map(e => [...e.split(' ').map(Number), 0]);
+let same = 1;
 
-while (que.length) {
-  const [cur, t] = que.shift();
-
-  if (cur === ed) {
-    console.log(t);
-    break;
-  }
-  
-  for (let d of [cur * 2, cur - 1 , cur + 1]) {
-    if (!visited[d] && d >= 0 && d < 100001) {
-      visited[d] = true;
-      if (d === cur * 2) que.unshift([d, t]);
-      else que.push([d, t + 1]);
+arr.sort((a, b) => {
+  if (b[1] > a[1]) return 1;
+  else if (b[1] === a[1]) {
+    if (b[2] > a[2]) return 1;
+    else if (b[2] === a[2]) {
+      if (b[3] > a[3]) return 1;
+      else if (b[3] === a[3]) return 0;
+      else return -1;
     }
+    else return -1;
+  }
+  else return -1;
+});
+
+arr[0][4] = 1;
+for (let i = 1; i < n; i++){
+  if (arr[i - 1][1] === arr[i][1] && arr[i - 1][2] === arr[i][2] && arr[i - 1][3] === arr[i][3]) {
+    arr[i][4] = arr[i - 1][4];
+    same++;
+  } else {
+    arr[i][4] = arr[i - 1][4] + same;
+    same = 1;
   }
 }
+
+console.log(arr.find(e => e[0] === k)[4]);
