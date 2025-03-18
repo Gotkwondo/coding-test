b9a28bbb61323339c1dd6ac6d8398d309120b818
오주현 <dhwngus99@gmail.com>
2025-03-18 21:23:01 +0900

    ♻️ refactor(gold 3): 경사로: 구현

--- BeakJoon/14890_경사로/14890.js
+++ BeakJoon/14890_경사로/14890.js
@@ -1,61 +1,114 @@
-const { link } = require('fs');
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/example.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
 
-const input = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
-  .toString().trim().split('\n').map(e => e.split(' ').map(Number))
-
-const [n, l] = input.shift();
-let map = input.slice();
+const [n, l] = input.shift().split(" ").map(Number);
+const map = input.map((e) => e.split(" ").map(Number));
 let answer = 0;
-let newMap = Array.from(Array(n), () => Array(n));
 
-for (let i = 0; i < n; i++){
-  const checkArr = map[i];
-  let sameL = 1;
-  
-  for (let j = 1; j < n; j++){
-    if (checkArr[j] === checkArr[j - 1]) sameL += 1;
-    else if (checkArr[j] - 1 === checkArr[j - 1] && sameL >= l) {
-      sameL = 1;
-    }
-    else if (checkArr[j] + 1 === checkArr[j - 1] && sameL >= 0) {
-      sameL = 1 - l;
-    }
-    else {
-      sameL = -1;
-      break;
+const check = (arr) => {
+  let used = Array(n).fill(false); // 경사로 설치 여부 체크
+  for (let i = 1; i < n; i++) {
+    if (arr[i] === arr[i - 1]) {
+      continue;
+    } else if (arr[i] - arr[i - 1] === 1) {
+      for (let j = i - 1; j >= i - l; j--) {
+        if (j < 0 || arr[j] !== arr[i - 1] || used[j]) return false;
+        used[j] = true;
+      }
+    } else if (arr[i] - arr[i - 1] === -1) {
+      for (let j = i; j < i + l; j++) {
+        if (j >= n || arr[j] !== arr[i] || used[j]) return false;
+        used[j] = true;
+      }
+    } else {
+      return false;
     }
   }
-  if (sameL >= 0) {
+  return true;
+};
+
+for (let i = 0; i < n; i++) {
+  const arr = map[i];
+  if (check(arr)) {
     answer += 1;
   }
 }
 
-for (let i = 0; i < n; i++){
-  for (let j = 0; j < n; j++){
-    newMap[j][i] = map[i][j];
+for (let i = 0; i < n; i++) {
+  let arr = [];
+  for (let j = 0; j < n; j++) {
+    arr.push(map[j][i]);
   }
-}
-
-for (let i = 0; i < n; i++){
-  const checkArr = newMap[i];
-  let sameL = 1;
-  
-  for (let j = 1; j < n; j++){
-    if (checkArr[j] === checkArr[j - 1]) sameL += 1;
-    else if (checkArr[j] - 1 === checkArr[j - 1] && sameL >= l) {
-      sameL = 1;
-    }
-    else if (checkArr[j] + 1 === checkArr[j - 1] && sameL >= 0) {
-      sameL = 1 - l;
-    }
-    else {
-      sameL = -1;
-      break;
-    }
-  }
-  if (sameL >= 0) {
+  if (check(arr)) {
     answer += 1;
   }
 }
+
 console.log(answer);
+
+// const { link } = require('fs');
+
+// const input = require('fs')
+//   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
+//   .toString().trim().split('\n').map(e => e.split(' ').map(Number))
+
+// const [n, l] = input.shift();
+// let map = input.slice();
+// let answer = 0;
+// let newMap = Array.from(Array(n), () => Array(n));
+
+// for (let i = 0; i < n; i++){
+//   const checkArr = map[i];
+//   let sameL = 1;
+
+//   for (let j = 1; j < n; j++){
+//     if (checkArr[j] === checkArr[j - 1]) sameL += 1;
+//     else if (checkArr[j] - 1 === checkArr[j - 1] && sameL >= l) {
+//       sameL = 1;
+//     }
+//     else if (checkArr[j] + 1 === checkArr[j - 1] && sameL >= 0) {
+//       sameL = 1 - l;
+//     }
+//     else {
+//       sameL = -1;
+//       break;
+//     }
+//   }
+//   if (sameL >= 0) {
+//     answer += 1;
+//   }
+// }
+
+// for (let i = 0; i < n; i++){
+//   for (let j = 0; j < n; j++){
+//     newMap[j][i] = map[i][j];
+//   }
+// }
+
+// for (let i = 0; i < n; i++){
+//   const checkArr = newMap[i];
+//   let sameL = 1;
+
+//   for (let j = 1; j < n; j++){
+//     if (checkArr[j] === checkArr[j - 1]) sameL += 1;
+//     else if (checkArr[j] - 1 === checkArr[j - 1] && sameL >= l) {
+//       sameL = 1;
+//     }
+//     else if (checkArr[j] + 1 === checkArr[j - 1] && sameL >= 0) {
+//       sameL = 1 - l;
+//     }
+//     else {
+//       sameL = -1;
+//       break;
+//     }
+//   }
+//   if (sameL >= 0) {
+//     answer += 1;
+//   }
+// }
+// console.log(answer);
--- BeakJoon/test.js
+++ BeakJoon/test.js
@@ -2,37 +2,47 @@ const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
   .toString().trim().split('\n');
 
-const n = +input.shift();
-const m = +input.shift();
-const road = Array.from({ length: n }, (_, i) => input[i].split(' ').map(Number));
-const path = input[n].split(' ').map(e => +e - 1);
-let tripped = Array.from({ length: n }, (_, i) => i);
-let answer = 'YES';
+const [n, l] = input.shift().split(' ').map(Number);
+const map = input.map(e => e.split(' ').map(Number));
+let answer = 0;
 
-if (path.length === 1) console.log("YES");
-else {
-  const find = (parent, x) => {
-    if (parent[x] === x) {
-      return x;
-    }
-    return (parent[x] = find(parent, parent[x]));
-  };
-
-  const union = (parent, st, ed) => {
-    const a = find(parent, st);
-    const b = find(parent, ed);
-    if (a > b) parent[a] = b;
-    else parent[b] = a;
-  };
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (i !== j && road[i][j] === 1) union(tripped, i, j);
+const check = (arr) => {
+  let used = Array(n).fill(false); // 경사로 설치 여부 체크
+  for (let i = 1; i < n; i++) {
+    if (arr[i] === arr[i - 1]) {
+      continue;
+    } else if (arr[i] - arr[i - 1] === 1) {
+      for (let j = i - 1; j >= i - l; j--){
+        if (j < 0 || arr[j] !== arr[i - 1] || used[j]) return false;
+        used[j] = true;
+      }
+    } else if (arr[i] - arr[i - 1] === -1) {
+      for (let j = i; j < i + l; j++){
+        if (j >= n || arr[j] !== arr[i] || used[j]) return false;
+        used[j] = true;
+      }
+    } else {
+      return false;
     }
   }
-
-  for (let i = 0; i < m - 1; i++){
-    if (find(tripped, path[i]) !== find(tripped, path[i + 1])) answer = 'NO';
-  }
-  console.log(answer);
+  return true;
 }
+
+for (let i = 0; i < n; i++){
+  const arr = map[i];
+  if (check(arr)) {
+    answer += 1;
+  }
+}
+
+for (let i = 0; i < n; i++){
+  let arr = [];
+  for (let j = 0; j < n; j++){
+    arr.push(map[j][i]);
+  }
+  if (check(arr)) {
+    answer += 1;
+  }
+}
+
+console.log(answer)
