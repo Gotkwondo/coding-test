fdcd2998e7ff833791cc4457c84f6e4c445d53eb
오주현 <dhwngus99@gmail.com>
2025-03-20 19:00:07 +0900

    ✨ feat(gold 5): 괄호의 값: 스택, 구현

--- BeakJoon/test.js
+++ BeakJoon/test.js
@@ -2,11 +2,37 @@ const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
   .toString().trim().split('\n');
 
-let st = input.shift();
-const list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
+let st = input.shift().split('');
+let stack = [];
+let temp = 1;
+let answer = 0;
+let flag = true;
 
-for (let cro of list) {
-  st = st.replaceAll(cro, 'q');
+for (let i = 0; i < st.length; i++){
+  const s = st[i];
+  if (s === '(' || s === '[') {
+    stack.push(s);
+    temp *= s === '(' ? 2 : 3;
+  }
+  else if (s === ')') {
+    if (stack.length === 0 || stack[stack.length - 1] !== '(') {
+      flag = false;
+      break;
+    } else if (st[i - 1] === '(') {
+      answer += temp;
+    }
+    stack.pop();
+    temp /= 2;
+  }
+  else if (s === ']') {
+    if (stack.length === 0 || stack[stack.length - 1] !== '[') {
+      flag = false;
+      break;
+    } else if (st[i - 1] === '[') {
+      answer += temp;
+    }
+    stack.pop();
+    temp /= 3;
+  }
 }
-
-console.log(st.length);
+console.log((stack.length > 0 || !flag) ? 0 : answer);
