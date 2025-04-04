f7d2a83b0c8ffc6d8c538dc4d7f0ab62fdf698d3
오주현 <dhwngus99@gmail.com>
2025-04-04 22:24:11 +0900

    ✨ feat(gold 4): 운동: 플로이드 워셜, 구현, 시뮬레이션

---
+++ BeakJoon/1956_운동/1956.js
@@ -0,0 +1,38 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/example.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const [v, e] = input.shift().split(" ").map(Number);
+const road = input.map((e) => e.split(" ").map(Number));
+const village = Array.from({ length: v }, (_, idx) =>
+  Array.from({ length: v }, (__, i) => (idx === i ? 0 : Infinity))
+);
+let answer = Infinity;
+
+for (let i = 0; i < e; i++) {
+  const [st, ed, val] = road[i];
+  village[st - 1][ed - 1] = val;
+}
+
+for (let i = 0; i < v; i++) {
+  for (let j = 0; j < v; j++) {
+    for (let k = 0; k < v; k++) {
+      village[j][k] = Math.min(village[j][k], village[j][i] + village[i][k]);
+    }
+  }
+}
+
+for (let i = 0; i < v; i++) {
+  for (let j = 0; j < v; j++) {
+    if (i === j) continue;
+    if (village[i][j] !== Infinity && village[j][i] !== Infinity) {
+      answer = Math.min(village[i][j] + village[j][i], answer);
+    }
+  }
+}
+
+console.log(answer === Infinity ? -1 : answer);
--- BeakJoon/test.js
+++ BeakJoon/test.js
@@ -2,123 +2,32 @@ const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
   .toString().trim().split('\n');
 
-const [n, m, x, y, k] = input.shift().split(' ').map(Number);
-const command = input.pop().split(' ').map(Number);
-const map = input.map(e => e.split(' ').map(Number));
-let answer = "";
-const dice = {
-  up: 0,
-  down: 0,
-  north: 0,
-  south: 0,
-  east: 0,
-  west: 0,
-  pos: [x, y]
-};
+const [v, e] = input.shift().split(' ').map(Number);
+const road = input.map(e => e.split(' ').map(Number));
+const village = Array.from({ length: v }, (_, idx) => Array.from({ length: v }, (__, i) => idx === i ? 0 : Infinity));
+let answer = Infinity;
 
-const dir = [null, [0, 1], [0, -1], [-1, 0], [1, 0]];
-
-const check = (command, curX, curY) => {
-  const [nextX, nextY] = [curX + dir[command][0], curY + dir[command][1]];
-  if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m) {
-    return false;
-  }
-  return true;
-};
-
-const roll = {
-  1: (curX, curY) => {
-    if (check(1, curX, curY)) return;
-    else {
-      const [nextX, nextY] = [curX + dir[1][0], curY + dir[1][1]];
-      dice.pos = [nextX, nextY];
-
-      [dice.up, dice.east, dice.down, dice.west] = [
-        dice.west,
-        dice.up,
-        dice.east,
-        dice.down,
-      ];
-
-      if(map[nextX][nextY] === 0){
-        map[nextX][nextY] = dice.down;
-      } else{
-        dice.down = map[nextX][nextY];
-        map[nextX][nextY] = 0;
-      }
-      answer += `${dice.up}\n`;
-    }
-  },
-  2: (curX, curY) => {
-    if (check(2, curX, curY)) return;
-    else {
-      const [nextX, nextY] = [curX + dir[2][0], curY + dir[2][1]];
-      dice.pos = [nextX, nextY];
-
-      [dice.up, dice.east, dice.down, dice.west] = [
-        dice.east,
-        dice.down,
-        dice.west,
-        dice.up,
-      ];
-
-      if(map[nextX][nextY] === 0){
-        map[nextX][nextY] = dice.down;
-      } else{
-        dice.down = map[nextX][nextY];
-        map[nextX][nextY] = 0;
-      }
-      answer += `${dice.up}\n`;
-    }
-  },
-  3: (curX, curY) => {
-    if (check(3, curX, curY)) return;
-    else {
-      const [nextX, nextY] = [curX + dir[3][0], curY + dir[3][1]];
-      dice.pos = [nextX, nextY];
-
-      [dice.up, dice.south, dice.down, dice.north] = [
-        dice.south,
-        dice.down,
-        dice.north,
-        dice.up,
-      ];
-
-      if(map[nextX][nextY] === 0){
-        map[nextX][nextY] = dice.down;
-      } else{
-        dice.down = map[nextX][nextY];
-        map[nextX][nextY] = 0;
-      }
-      answer += `${dice.up}\n`;
-    }
-  },
-  4: (curX, curY) => {
-    if (check(4, curX, curY)) return;
-    else {
-      const [nextX, nextY] = [curX + dir[4][0], curY + dir[4][1]];
-      dice.pos = [nextX, nextY];
-
-      [dice.up, dice.south, dice.down, dice.north] = [
-        dice.north,
-        dice.up,
-        dice.south,
-        dice.down,
-      ];
-
-      if(map[nextX][nextY] === 0){
-        map[nextX][nextY] = dice.down;
-      } else{
-        dice.down = map[nextX][nextY];
-        map[nextX][nextY] = 0;
-      }
-      answer += `${dice.up}\n`;
-    }
-  },
-};
-
-for (let i = 0; i < k; i++){
-  roll[command[i]](dice.pos[0], dice.pos[1]);
+for (let i = 0; i < e; i++){
+  const [st, ed, val] = road[i];
+  village[st - 1][ed - 1] = val;
 }
 
-console.log(answer)
+for (let i = 0; i < v; i++){
+  for (let j = 0; j < v; j++){
+    for (let k = 0; k < v; k++){
+      village[j][k] = Math.min(village[j][k], village[j][i] + village[i][k]);
+    }
+  }
+}
+
+for (let i = 0; i < v; i++){
+  for (let j = 0; j < v; j++){
+    if (i === j) continue;
+    if (village[i][j] !== Infinity && village[j][i] !== Infinity) {
+      answer = Math.min(village[i][j] + village[j][i], answer);
+    }
+    
+  }
+}
+
+console.log(answer === Infinity ? -1 : answer);
