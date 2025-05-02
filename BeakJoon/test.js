a9d7eefa10315df438ec38e2e324057bbadaff44
오주현 <dhwngus99@gmail.com>
2025-05-02 21:06:17 +0900

    ✨ feat(gold 5): 숫자고르기: DFS, 사이클, 그래프 탐색

--- BeakJoon/test.js
+++ BeakJoon/test.js
@@ -7,179 +7,35 @@ const input = require("fs")
   .split("\n")
   .map((e) => e.trim());
 
-const [n, m] = input.shift().split(' ');
-const map = input.map(line => line.split(' '));
-const cam = [];
-let zero = 0;
-for (let i = 0; i < n; i++){
-  for (let j = 0; j < m; j++){
-    if (map[i][j] === '0') zero += 1;
-    else if (map[i][j] === '6') continue;
-    else cam.push([i, j, map[i][j]]);
-  }
-}
-let answer = 0;
+const n = +input[0];
+const table = Array.from({ length: n + 1 }, (_, i) => +input[i]);
 
-const copyMap = (map) => {
-  const newMap = Array.from({ length: n }, () => Array.from({ length: m }, () => '0'));
-  for (let i = 0; i < n; i++){
-    for (let j = 0; j < m; j++){
-      if (map[i][j] === '0') continue;
-      else newMap[i][j] = map[i][j];
-    }
-  }
-  return newMap;
-}
+const answer = [];
+const visited = Array(n + 1).fill(false);
+const finished = Array(n + 1).fill(false);
 
-const search = {
-  up: (y, x, checkMap) => {
-    let curY = y - 1;
-    let cnt = 0;
-    while (curY >= 0) {
-      if (checkMap[curY][x] === '0') {
-        checkMap[curY][x] = '#';
-        cnt += 1;
-        curY -= 1;
-      } else if (checkMap[curY][x] === '6') {
-        break;
-      } else curY -= 1;
+function dfs(start, current) {
+  visited[current] = true;
+  const next = table[current];
+
+  if (!visited[next]) {
+    dfs(start, next);
+  } else if (!finished[next]) {
+    // 사이클이 형성되었을 경우만 저장
+    if (next === start) {
+      answer.push(start);
     }
-    return cnt;
-  },
-  right: (y, x, checkMap) => {
-    let curX = x + 1;
-    let cnt = 0;
-    while (curX < m) {
-      if (checkMap[y][curX] === '0') {
-        checkMap[y][curX] = '#';
-        cnt += 1;
-        curX += 1;
-      } else if (checkMap[y][curX] === '6') {
-        break;
-      } else curX += 1;
-    }
-    return cnt;
-  },
-  left: (y, x, checkMap) => {
-    let curX = x - 1;
-    let cnt = 0;
-    while (curX >= 0) {
-      if (checkMap[y][curX] === '0') {
-        checkMap[y][curX] = '#';
-        cnt += 1;
-        curX -= 1;
-      } else if (checkMap[y][curX] === '6') {
-        break;
-      } else curX -= 1;
-    }
-    return cnt;
-  },
-  down: (y, x, checkMap) => {
-    let curY = y + 1;
-    let cnt = 0;
-    while (curY < n) {
-      if (checkMap[curY][x] === '0') {
-        checkMap[curY][x] = '#';
-        cnt += 1;
-        curY += 1;
-      } else if (checkMap[curY][x] === '6') {
-        break;
-      } else curY += 1;;
-    }
-    return cnt;
   }
+
+  finished[current] = true;
 }
 
-const dfs = (cnt, depth, copiedMap) => {
-  if (depth === cam.length) {
-    answer = Math.max(answer, cnt);
-    return;
-  }
-
-  const [cy, cx, type] = cam[depth];
-  if (type === '1') {
-    for (let i = 0; i < 4; i++){
-      const newMap = copyMap(copiedMap);
-      if (i === 0) {
-        let newCnt = search.up(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 1) {
-        let newCnt = search.right(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 2) {
-        let newCnt = search.down(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 3) {
-        let newCnt = search.left(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-    }
-  }
-  else if (type === '2') {
-    for (let i = 0; i < 2; i++){
-      const newMap = copyMap(copiedMap);
-      if (i === 0) {
-        let newCnt = search.up(cy, cx, newMap) + search.down(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 1) {
-        let newCnt = search.right(cy, cx, newMap) + search.left(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-    }
-  }
-  else if (type === '3') {
-    for (let i = 0; i < 4; i++){
-      const newMap = copyMap(copiedMap);
-      if (i === 0) {
-        let newCnt = search.up(cy, cx, newMap) + search.right(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 1) {
-        let newCnt = search.down(cy, cx, newMap) + search.right(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 2) {
-        let newCnt = search.down(cy, cx, newMap) + search.left(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 3) {
-        let newCnt = search.up(cy, cx, newMap) + search.left(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-    }
-  }
-  else if (type === '4') {
-    for (let i = 0; i < 4; i++){
-      const newMap = copyMap(copiedMap);
-      if (i === 0) {
-        let newCnt = search.up(cy, cx, newMap) + search.left(cy, cx, newMap) + search.right(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 1) {
-        let newCnt = search.up(cy, cx, newMap) + search.right(cy, cx, newMap) + search.down(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 2) {
-        let newCnt = search.down(cy, cx, newMap) + search.left(cy, cx, newMap) + search.right(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-      else if (i === 3) {
-        let newCnt = search.up(cy, cx, newMap) + search.left(cy, cx, newMap) + search.down(cy, cx, newMap);
-        dfs(cnt + newCnt, depth + 1, newMap);
-      }
-    }
-  }
-  else if (type === '5') {
-    const newMap = copyMap(copiedMap);
-    let newCnt = search.up(cy, cx, newMap) + search.left(cy, cx, newMap) + search.down(cy, cx, newMap) + search.right(cy, cx, newMap);
-    dfs(cnt + newCnt, depth + 1, newMap);
-  }
+for (let i = 1; i <= n; i++) {
+  visited.fill(false);
+  finished.fill(false);
+  dfs(i, i);
 }
 
-dfs(0, 0, map);
-
-console.log(zero - answer);
+answer.sort((a, b) => a - b);
+console.log(answer.length);
+console.log(answer.join("\n"));
