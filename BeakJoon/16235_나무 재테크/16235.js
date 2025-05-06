0be68113eb0064177c33deea936d2ee9df203537
오주현 <dhwngus99@gmail.com>
2025-05-06 22:06:38 +0900

    ✨ feat(glod 3): 나무 재태크: 구

--- BeakJoon/16235_나무 재테크/16235.js
+++ BeakJoon/16235_나무 재테크/16235.js
@@ -1,69 +1,175 @@
-const input = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
-  .toString().trim().split('\n').map(e => e.split(' ').map(Number));
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/example.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n")
+  .map((e) => e.trim());
 
-const [n, m, k] = input.shift();
-let plusHp = input.splice(0, n);
-let trees = input.map(e => [...e, 0]);
-let map = Array.from(Array(n), () => Array(n).fill(5));
+const [n, m, k] = input.shift().split(" ").map(Number);
+const food = input.slice(0, n).map((e) => e.split(" ").map(Number));
+const trees = input
+  .slice(n, input.length + 1)
+  .map((e) => e.split(" ").map(Number));
+const map = Array.from({ length: n }, () =>
+  Array.from({ length: n }, () => {
+    return { food: 5, tree: [], dead: [] };
+  })
+);
 const dir = [
-  [-1, -1], [0, -1], [1, -1], [1, 0],
-  [1, 1], [0, 1], [-1, 1], [-1, 0]
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
 ];
 
+for (let [y, x, age] of trees) {
+  map[y - 1][x - 1].tree.push(age);
+}
+
 const spring = () => {
-  trees = trees.sort((a, b) => a[2] - b[2]);
-  for (let i = 0; i < trees.length; i++){
-    const [x, y, age] = trees[i];
-    if (map[x - 1][y - 1] >= age) {
-      map[x - 1][y - 1] -= age;
-      trees[i][2] += 1;
-    }
-    else {
-      trees[i][3] = -1;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      map[i][j].tree.sort((a, b) => a - b);
+      const newTrees = [];
+      for (let k = 0; k < map[i][j].tree.length; k++) {
+        const t = map[i][j].tree[k];
+        if (map[i][j].food >= t) {
+          map[i][j].food -= t;
+          newTrees.push(t + 1);
+        } else {
+          map[i][j].dead.push(t);
+        }
+      }
+      map[i][j].tree = newTrees;
     }
   }
-  
-}
+};
 
 const summer = () => {
-  trees = trees.filter(e => {
-    if (e[3] === 0) return true;
-    else {
-      const [x, y, age, _] = e;
-      map[x - 1][y - 1] += Math.floor(age / 2);
-      return false;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (map[i][j].dead.length === 0) continue;
+      let sum = 0;
+      for (let d of map[i][j].dead) {
+        sum += Math.floor(d / 2);
+      }
+      map[i][j].food += sum;
+      map[i][j].dead = [];
     }
-  });
-}
+  }
+};
 
-const automn = () => {
-  trees.forEach(e => {
-    const [x, y, age, _] = e;
-    if (age % 5 === 0) {
-      for (let i = 0; i < 8; i++) {
-        const nY = y - 1 + dir[i][1];
-        const nX = x - 1 + dir[i][0];
-        if (nY >= 0 && nY < n && nX >= 0 && nX < n) {
-          trees.push([nX + 1, nY + 1, 1, 0]);
+const autumn = () => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let t of map[i][j].tree) {
+        if (t % 5 === 0) {
+          for (let [dy, dx] of dir) {
+            const [ny, nx] = [i + dy, j + dx];
+            if (ny >= 0 && ny < n && nx >= 0 && nx < n) {
+              map[ny][nx].tree.push(1);
+            }
+          }
         }
       }
     }
-  })
-}
+  }
+};
 
 const winter = () => {
-  for (let i = 0; i < n; i++){
-    for (let j = 0; j < n; j++){
-      map[i][j] += plusHp[i][j];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      map[i][j].food += food[i][j];
     }
   }
-}
+};
 
-for (let i = 0; i < k; i++){
+for (let i = 0; i < k; i++) {
   spring();
   summer();
-  automn();
+  autumn();
   winter();
 }
-console.log(trees.length);
+
+console.log(
+  map.reduce(
+    (acc, cur) => acc + cur.reduce((ac, cu) => ac + cu.tree.length, 0),
+    0
+  )
+);
+
+// const input = require('fs')
+//   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/example.txt')
+//   .toString().trim().split('\n').map(e => e.split(' ').map(Number));
+
+// const [n, m, k] = input.shift();
+// let plusHp = input.splice(0, n);
+// let trees = input.map(e => [...e, 0]);
+// let map = Array.from(Array(n), () => Array(n).fill(5));
+// const dir = [
+//   [-1, -1], [0, -1], [1, -1], [1, 0],
+//   [1, 1], [0, 1], [-1, 1], [-1, 0]
+// ];
+
+// const spring = () => {
+//   trees = trees.sort((a, b) => a[2] - b[2]);
+//   for (let i = 0; i < trees.length; i++){
+//     const [x, y, age] = trees[i];
+//     if (map[x - 1][y - 1] >= age) {
+//       map[x - 1][y - 1] -= age;
+//       trees[i][2] += 1;
+//     }
+//     else {
+//       trees[i][3] = -1;
+//     }
+//   }
+
+// }
+
+// const summer = () => {
+//   trees = trees.filter(e => {
+//     if (e[3] === 0) return true;
+//     else {
+//       const [x, y, age, _] = e;
+//       map[x - 1][y - 1] += Math.floor(age / 2);
+//       return false;
+//     }
+//   });
+// }
+
+// const automn = () => {
+//   trees.forEach(e => {
+//     const [x, y, age, _] = e;
+//     if (age % 5 === 0) {
+//       for (let i = 0; i < 8; i++) {
+//         const nY = y - 1 + dir[i][1];
+//         const nX = x - 1 + dir[i][0];
+//         if (nY >= 0 && nY < n && nX >= 0 && nX < n) {
+//           trees.push([nX + 1, nY + 1, 1, 0]);
+//         }
+//       }
+//     }
+//   })
+// }
+
+// const winter = () => {
+//   for (let i = 0; i < n; i++){
+//     for (let j = 0; j < n; j++){
+//       map[i][j] += plusHp[i][j];
+//     }
+//   }
+// }
+
+// for (let i = 0; i < k; i++){
+//   spring();
+//   summer();
+//   automn();
+//   winter();
+// }
+// console.log(trees.length);
--- BeakJoon/test.js
+++ BeakJoon/test.js
@@ -7,52 +7,79 @@ const input = require("fs")
   .split("\n")
   .map((e) => e.trim());
 
-// n = 가로 세로 길이, m = 치킨집의 최대 갯수
-const [n, m] = input.shift().split(" ").map(Number);
-const map = input.map((e) => e.split(" ").map(Number));
-const houses = [];
-const chickens = [];
-const chickenCheck = [];
-let answer = Infinity;
+const [n, m, k] = input.shift().split(" ").map(Number);
+const food = input.slice(0, n).map(e => e.split(' ').map(Number));
+const trees = input.slice(n, input.length + 1).map(e => e.split(' ').map(Number));
+const map = Array.from({ length: n }, () => Array.from({ length: n }, () => { return { food: 5, tree: [], dead: [] } }));
+const dir = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]];
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    if (map[i][j] === 1) houses.push([i, j]);
-    else if (map[i][j] === 2) {
-      chickens.push([i, j]);
-      chickenCheck.push(false);
-    }
-  }
+for (let [y, x, age] of trees) {
+  map[y - 1][x - 1].tree.push(age);
 }
 
-const calcDistance = ([cy, cx], [hy, hx]) => {
-  return Math.abs(cy - hy) + Math.abs(cx - hx);
-};
-
-const dfs = (depth, index) => {
-  if (depth === m) {
-    let sum = 0;
-    for (let house of houses) {
-      let min = Infinity;
-      for (let c = 0; c < chickenCheck.length; c++) {
-        if (!chickenCheck[c]) continue; 
-        const chicken = chickens[c];
-        const distance = calcDistance(chicken, house);
-        if (distance < min) min = distance;
+const spring = () => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      map[i][j].tree.sort((a, b) => a - b);
+      const newTrees = [];
+      for (let k = 0; k < map[i][j].tree.length; k++) {
+        const t = map[i][j].tree[k];
+        if (map[i][j].food >= t) {
+          map[i][j].food -= t;
+          newTrees.push(t + 1);
+        } else {
+          map[i][j].dead.push(t);
+        }
       }
-      sum += min;
-    }
-    if (answer > sum) answer = sum;
-    return;
-  }
-  for (let i = index; i < chickenCheck.length; i++){
-    if (!chickenCheck[i]) {
-      chickenCheck[i] = true;
-      dfs(depth + 1, i);
-      chickenCheck[i] = false;
+      map[i][j].tree = newTrees;
     }
   }
 };
 
-dfs(0, 0);
-console.log(answer)
+const summer = () => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (map[i][j].dead.length === 0) continue;
+      let sum = 0;
+      for (let d of map[i][j].dead) {
+        sum += (Math.floor(d / 2));
+      }
+      map[i][j].food += sum;
+      map[i][j].dead = [];
+    }
+  }
+};
+
+const autumn = () => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let t of map[i][j].tree) {
+        if (t % 5 === 0) {
+          for (let [dy, dx] of dir) {
+            const [ny, nx] = [i + dy, j + dx];
+            if (ny >= 0 && ny < n && nx >= 0 && nx < n) {
+              map[ny][nx].tree.push(1);
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const winter = () => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      map[i][j].food += food[i][j];
+    }
+  }
+};
+
+for (let i = 0; i < k; i++){
+  spring();
+  summer();
+  autumn();
+  winter();
+}
+
+console.log(map.reduce((acc, cur) => acc + cur.reduce((ac, cu) => ac + cu.tree.length, 0), 0));
